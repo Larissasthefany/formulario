377b06ab1cf4511fe70f8802d85a803fa107c226
larissasthefany <larigithub@gmail.com>
2023-11-26 19:59:26 -0300

    Adiciona efeito de hover e transição suave ao botão de cadastro

--- src/register.jsx
+++ src/register.jsx
@@ -1,5 +1,6 @@
 import { Component } from "react"
 import './register.css'
+import Login from '../public/image/login.png'
 class Register extends Component {
     constructor(props) {
         super(props);
@@ -44,35 +45,43 @@ class Register extends Component {
 
     render() {
         return (
-            <div>
-                <h1>Cadastre-se</h1>
+            <div className="container-form">
                 {this.state.error && <p>{this.state.error}</p>}
                 <form onSubmit={this.cadastrar}>
-                    <label>Nome:</label>
-                    <input type="text" name="nome" value={this.state.form.nome}
-                        onChange={this.dadosForm} />
-                    <br />
+                    <legend>Cadastre - se</legend>
 
-                    <label>Sobrenome:</label>
-                    <input type="text" name="sobrenome" value={this.state.form.sobrenome} onChange={this.dadosForm} />
-                    <br />
+                    <div className="input-group">
+                        <input className="input" type="text" name="nome" value={this.state.form.nome}
+                            onChange={this.dadosForm} />
+                        <label className="label">Nome</label>
+                    </div>
 
-                    <label>Email:</label>
-                    <input type="email" name="email" value={this.state.form.email}
-                        onChange={this.dadosForm} />
-                    <br />
+                    <div className="input-group">
+                        <input className="input" type="text" name="sobrenome" value={this.state.form.sobrenome} onChange={this.dadosForm} />
+                        <label className="label">Sobrenome</label>
+                    </div>
 
-                    <label>Senha:</label>
-                    <input type="password" name="senha" value={this.state.form.senha}
-                        onChange={this.dadosForm} />
-                    <br />
+                    <div className="input-group">
+                        <input className="input" type="email" name="email" value={this.state.form.email}
+                            onChange={this.dadosForm} />
+                        <label className="label">Email</label>
+                    </div>
+
+                    <div className="input-group">
+                        <input className="input" type="password" name="senha" value={this.state.form.senha}
+                            onChange={this.dadosForm} />
+                        <label className="label">Senha</label>
+                    </div>
 
                     <input type="checkbox" name="aceitarTermo" id="aceitarTermo" checked={this.state.form.aceitarTermo} onChange={this.dadosForm} />
-                    <label htmlFor="aceitarTermo">Eu aceito os Termos e condições de Politica privacidade.</label>
+                    <label htmlFor="aceitarTermo"> Eu aceito os Termos e condições de <span className="termo">Politica</span> de <span className="termo">privacidade</span>.</label>
                     <br />
 
                     <button type="submit" className="button-48"><span className="text">Cadastrar</span></button>
                 </form>
+                <div>
+                    <img src={Login} alt="Imagem de login" />
+                </div>
             </div>
         );
     }
